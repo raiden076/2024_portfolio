118dd3937de26f9594363ad4836057a6d58ce9f0
arkaprav0 <mail@arkaprav0.in>
2024-07-19 00:50:15 +0530

    cf to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import cloudflare from "@astrojs/cloudflare";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   integrations: [tailwind()],
   output: "hybrid",
-  adapter: cloudflare(),
+  adapter: netlify({
+    edgeMiddleware: true
+  })
 });
