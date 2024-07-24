27a6e1c68e42fb677eb6c54c819f50a63857626a
arkaprav0 <mail@arkaprav0.in>
2024-07-24 20:56:38 +0530

    should do it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   integrations: [tailwind()],
   output: "hybrid",
-  adapter: netlify({
-    edgeMiddleware: true
-  })
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
