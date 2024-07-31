879048866a6cf4179b9aba2fda569b17b655fb9e
arkaprav0 <mail@arkaprav0.in>
2024-07-31 23:49:53 +0530

    done-85

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,7 +5,7 @@ const postCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     tags: z.array(z.string()),
-    // image: z.string().optional(),
+    featured: z.boolean().optional(),
     description: z.string(),
     publishedAt: z.date().default(() => new Date()),
   }),
