8ae0f6204984039268a518cb3f6c0689a338e9b3
arkaprav0 <mail@arkaprav0.in>
2024-08-14 17:38:14 +0530

    calculators-solid-init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
-
 import mdx from "@astrojs/mdx";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), solidJs()],
   output: "hybrid",
   adapter: cloudflare({
     platformProxy: {
---
+++ src/components/calculators/main.jsx
@@ -0,0 +1,11 @@
+export function CalcApp() {
+  return (
+    <div>
+      <MyComponent />
+    </div>
+  );
+}
+
+function MyComponent() {
+  return <div>Hello World</div>;
+}
